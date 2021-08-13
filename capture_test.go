bbe24aba5b5372cef25a85107010060c38bc0af9
Joinhack <Joinhack@gmail.com>
2021-08-13 22:59:33 +0800

    init

--- capture_test.go
+++ capture_test.go
@@ -27,12 +27,18 @@ func TestCapture(t *testing.T) {
 	f.Convert(w, h)
 	rgb := i420ToRgb(w, h, f.data)
 	file, _ := os.Create("image.png")
-	dst := []byte{}
+	dst := make([]byte, w*h*4)
 	stride := len(rgb) / h;
+	idx := 0
 	for y :=0; y < h; y++ {
 		for x :=0; x < w; x++ {
 			i := stride * y + 3 * x
-			dst = append(dst, []byte{rgb[i], rgb[i+1], rgb[i+2], 255}...)
+			dst[idx] = rgb[i]
+			dst[idx+1] = rgb[i+1]
+			dst[idx+2] = rgb[i+2]
+			dst[idx+3] = 255
+			idx += 4
+			
 		}
 	}
 	img.Pix = dst
