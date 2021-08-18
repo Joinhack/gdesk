02946600dd2842c3199563927a446b19cf07e991
Joinhack <Joinhack@gmail.com>
2021-08-18 16:27:56 +0800

    init

--- codec_test.go
+++ codec_test.go
@@ -3,7 +3,7 @@ package gdesk
 
 import (
 	"testing"
-	"time"
+	// "time"
 )
 
 func TestCodec(t *testing.T) {
@@ -18,16 +18,17 @@ func TestCodec(t *testing.T) {
 	for ii := 0; ii < 100; ii++ {
 		f = c.GetFrame()
 		if f != nil {
-			defer f.Release()
 			te := time.Now()
 			_ = uint64(te.UnixNano() - ts.UnixNano())
-			encodeFrames := encoder.Encode(f, uint64(ii))
+			_ = encoder.Encode(f, uint64(ii))
 			var frame *EncodeFrame
 			for frame = encodeFrames.Next(); frame != nil; frame = encodeFrames.Next() {
 				writer.WriteFrame(frame)
 			}
 			time.Sleep(10 * time.Millisecond)
+			f.Release()
 		}
+		
 	}
 	writer.Close()
 }
